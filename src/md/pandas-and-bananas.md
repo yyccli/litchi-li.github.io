97a69c26971cf410e8fa0bb7add8529ec50100d6
Bob YC Li/WSH/Wistron <Bob_YC_Li@wistron.com>
2019-11-29 10:30:26 +0800

    start update index

--- src/md/pandas-and-bananas.md
+++ src/md/pandas-and-bananas.md
@@ -1,6 +1,6 @@
 ---
-title: "Pandas and Bananas and lucky dog"
-date: "2017-08-21"
+title: "Pandas and Bananas and lucky dog ..."
+date: "2016-08-21"
 ---
 
 Do Pandas eat bananas? Check out this short video that shows that yes! pandas do
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,17 @@ export default ({data}) => {
     
     return (
     <Layout>
+        <div>
+            <h3>Description</h3>
+        </div>
+
+        <div>
+            <h3>What i have done</h3>
+        </div>
+
+        <div>
+            <h3>What i want to do</h3>
+        </div>
 
     </Layout>
     )
